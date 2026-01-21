4b96d89ac4f1aab3ea12dd0a5ed1191802e7339f
Hong-Phuc-Cao <hongphuccaowork@gmail.com>
2026-01-21 20:20:58 +0700

    Update link

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // Cấu hình @ → src/
     },
   },
-  base: '/Development-of-a-teaching-assignment-management-system-for-high-schools/'
+  base: '/Thesis-UI-demo/'
 })
 
 
